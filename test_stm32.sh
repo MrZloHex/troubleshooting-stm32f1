e301b1d3b589f761b52098542178665159d6cf86
MrZloHex <zlo.alex.it@gmail.com>
2021-09-16 20:58:15 +0000

    WORKS FINE

--- test_stm32.sh
+++ test_stm32.sh
@@ -1,1 +1,41 @@
 #!/bin/bash
+
+######################
+#  Made by MrZloHex  #
+#     16.09.2021     #
+######################
+
+ID=0
+
+make_elf() {
+	make clean
+	make
+}
+
+startup_stlink() {
+	st-util
+}
+
+debug() {
+	startup_stlink &
+	STLINK_PID=$!
+
+	arm-none-eabi-gdb --command=gdb.commands main.elf > ./device_$ID/test_output
+	
+	kill STLINK_PID
+}
+
+copy_dumps() {
+	mv flash.dump ./device_$ID/
+	mv sram.dump ./device_$ID/
+}
+
+main() {
+	make_elf
+	ID=$1
+	mkdir "device_$ID"
+	debug
+	copy_dumps
+}
+
+main "$@"
